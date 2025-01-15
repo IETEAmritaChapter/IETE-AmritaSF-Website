c34f2ec5ff8e31e8f74a363556184c4885327879
S Sajeev <167018420+SajeevSenthil@users.noreply.github.com>
2025-01-15 22:22:58 +0530

    Update Navigation.jsx

--- src/app/components/navbar/Navigation.jsx
+++ src/app/components/navbar/Navigation.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import NavigationItem from "./NavigationItem";
 
 export default function Navigation() {
@@ -11,7 +11,7 @@ export default function Navigation() {
   ];
 
   return (
-    <nav className="flex gap-20 text-xl font-semibold text-white">
+    <nav className="flex flex-col gap-4 md:flex-row md:gap-10 text-base md:text-xl font-semibold text-white [font-family:var(--font-montserrat)]">
       {navItems.map((item, index) => (
         <NavigationItem key={index} text={item} />
       ))}
