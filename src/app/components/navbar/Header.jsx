0e51e2eb741ea199bf3b1465f175eb813a8e98b9
S Sajeev <167018420+SajeevSenthil@users.noreply.github.com>
2025-01-15 22:17:21 +0530

    Update Header.jsx

--- src/app/components/navbar/Header.jsx
+++ src/app/components/navbar/Header.jsx
@@ -1,12 +1,28 @@
-import * as React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="flex items-center justify-start w-full px-2 md:px-4 h-16">
-      <Logo />
-      <div className="ml-auto">
+    <div className="flex flex-col md:flex-row items-center justify-between w-full px-2 md:px-4 py-2 [font-family:var(--font-montserrat)]">
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <Logo />
+        <button
+          className="md:hidden text-white text-2xl"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? "✕" : "☰"}
+        </button>
+      </div>
+      <div
+        className={`w-full md:w-auto md:flex ${
+          menuOpen ? "block" : "hidden"
+        } mt-4 md:mt-0`}
+      >
         <Navigation />
       </div>
     </div>
