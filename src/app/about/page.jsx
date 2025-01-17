120a2be8d834d99e776f1ae0c8b8084dfd258eed
Samhitha S <samhithas04@gmail.com>
2025-01-17 16:37:16 +0530

    Added Footer and updated About page with the changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,11 +11,11 @@ export default function Home() {
 
         {/* About Section */}
         <section className="about flex flex-col gap-4 items-center">
-          <h1 className="text-[60px] sm:text-[48px] font-extrabold font-[montserratb]">
+          <h2 className="text-[38px] sm:text-[40px] font-[montserratb]">
             ABOUT <span className="gradient-text">IETE AMRITA</span>
-          </h1>
+          </h2>
 
-          <div className="message-box text-base sm:text-lg lg:text-xl xl:text-2xl text-black p-6 bg-white rounded-[8px] text-start font-[montserrat] mt-4">
+          <div className="message-box text-black text-base sm:text-lg lg:text-xl xl:text-[1.3rem] p-6 bg-white rounded-[8px] text-start font-[montserrat] mt-4">
             At IETE Amrita, we are more than just a club; we are a thriving ecosystem for technology enthusiasts and innovators. Rooted in a passion for electronics, communication, and programming, we aim to bridge the gap between theoretical knowledge and real-world applications. Our mission is to foster a culture of innovation, empowering students to explore emerging technologies and push the boundaries of what&apos;s possible.
           </div>
         </section>
@@ -25,24 +25,42 @@ export default function Home() {
           <h2 className="text-[28px] sm:text-[32px] font-[montserratb]">
             <span className="text-orange-600">WHY</span> WE DO <span className="text-orange-600">WHAT</span> WE DO
           </h2>
-          <p className="text-base sm:text-lg lg:text-xl xl:text-2xl text-gray-400 montserrat-alternates text-center font-[montserrat]">
+          <p className="text-base sm:text-lg lg:text-xl xl:text-[1.3rem] text-gray-400 text-center font-[montserrat]">
             At IETE Amrita, our purpose stems from a shared passion for technology and innovation. Every action, initiative, and event we undertake is guided by a deep commitment to empowering students and fostering a culture of technical excellence.
           </p>
-          <div className="box-wrapper flex flex-wrap md:flex-nowrap flex-col md:flex-row justify-center items-stretch gap-4 w-full mt-4">
+          <div className="box-wrapper flex flex-wrap md:flex-row justify-center items-stretch gap-4 w-full mt-4">
             {/* Mission Box */}
-            <div className="box-container bg-white p-4 border rounded-[8px] shadow text-black flex-1 min-h-[150px]">
-              <h2 className="text-[28px] sm:text-[28px] text-start text-orange-600 font-[montserratb]">MISSION</h2>
-              <p className="text-base sm:text-lg lg:text-xl xl:text-2xl text-start font-[montserrat] mt-3">
-                Our mission is to organize impactful events and workshops that demystify emerging technologies, inspiring students to learn and innovate, while offering hands-on project opportunities that bridge the gap between theoretical knowledge and practical application to empower them with industry-relevant skills. We aim to foster an environment that encourages continuous learning.
-              </p>
+            <div className="box-container bg-white p-4 border rounded-[8px] shadow text-black flex-1 min-h-[150px] relative">
+              <img
+                src="/images/mission-vision/mission.png"
+                alt="Mission Icon"
+                className="w-11 h-11 absolute top-4 right-12"
+              />
+              <div className="text-start">
+                <h2 className="text-[26px] sm:text-[28px] text-orange-600 font-[montserratb]">
+                  MISSION
+                </h2>
+                <p className="text-base sm:text-lg lg:text-xl xl:text-[1.2rem] text-start font-[montserrat] mt-5">
+                  Our mission is to organize impactful events and workshops that demystify emerging technologies, inspiring students to learn and innovate, while offering hands-on project opportunities that bridge the gap between theoretical knowledge and practical application to empower them with industry-relevant skills. We aim to foster an environment that encourages continuous learning.
+                </p>
+              </div>
             </div>
 
             {/* Vision Box */}
-            <div className="box-container bg-white p-4 border rounded-[8px] shadow text-black flex-1 min-h-[150px]">
-              <h2 className="text-[28px] sm:text-[28px] font-bold text-start text-orange-600 font-[montserratb]">VISION</h2>
-              <p className="text-base sm:text-lg lg:text-xl xl:text-2xl text-start font-[montserrat] mt-3">
-                To create a vibrant and inclusive tech community at Amrita that inspires innovation, nurtures talent, and serves as a catalyst for technical creativity and collaboration. We envision IETE as a central hub where students explore the frontiers of technology, develop groundbreaking ideas, and collectively shape the future of engineering and innovation.
-              </p>
+            <div className="box-container bg-white p-4 border rounded-[8px] shadow text-black flex-1 min-h-[150px] relative">
+              <img
+                src="/images/mission-vision/vision.png"
+                alt="Vision Icon"
+                className="w-14 h-10 absolute top-4 right-12"
+              />
+              <div className="text-start">
+                <h2 className="text-[26px] sm:text-[28px] text-orange-600 font-[montserratb]">
+                  VISION
+                </h2>
+                <p className="text-base sm:text-lg lg:text-xl xl:text-[1.2rem] text-start font-[montserrat] mt-5">
+                  To create a vibrant and inclusive tech community at Amrita that inspires innovation, nurtures talent, and serves as a catalyst for technical creativity and collaboration. We envision IETE as a central hub where students explore the frontiers of technology, develop groundbreaking ideas, and collectively shape the future of engineering and innovation.
+                </p>
+              </div>
             </div>
           </div>
         </section>
@@ -52,36 +70,88 @@ export default function Home() {
           <h2 className="text-[28px] sm:text-[32px] font-[montserratb]">
             VARIOUS <span className="text-orange-600">VERTICALS</span> & <span className="text-orange-600">DOMAINS</span> WE EXPLORE
           </h2>
-          <p className="text-base sm:text-lg lg:text-xl xl:text-2xl text-gray-400 text-center font-[montserrat]">
+          <p className="text-base sm:text-lg lg:text-xl xl:text-[1.3rem] text-gray-400 text-center font-[montserrat]">
             Whether it&apos;s mastering Competitive Programming, pioneering the future with IoT, crafting dynamic Web Development, or unlocking the potential of AI/ML, each vertical is designed to spark passion, refine skills, and push boundaries.
           </p>
-          <div className="icons-grid grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-5 gap-6 w-full justify-items-center items-center mt-4">
-            {/* First Row */}
-            <img src="/images/verticals/webdev.png" alt="Web Development" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/embedded.png" alt="Embedded Systems" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/cp.png" alt="Competitive Programming" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/appdev.png" alt="App Development" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/ai.png" alt="AI & ML" className="w-28 h-28 sm:w-18 sm:h-18" />
+          <div className="icons-grid grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 w-full gap-6 justify-items-center items-center mt-4 font-[montserrat]">
+            {/* Web Development */}
+            <div className="text-center">
+              <img src="/images/verticals/webdev.png" alt="Web Development" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Web<br />Development</p>
+            </div>
 
-            {/* Second Row */}
-            <img src="/images/verticals/pr.png" alt="Public Relations" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/eventmang.png" alt="Event Management" className="w-28 h-28 sm:w-18 sm:h-18" />
-            <img src="/images/verticals/design.png" alt="Design" className="w-28 h-28 sm:w-18 sm:h-18" />
+            {/* Embedded Systems */}
+            <div className="text-center">
+              <img src="/images/verticals/embedded.png" alt="Embedded Systems" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Embedded <br />Systems</p>
+            </div>
+
+            {/* Competitive Programming */}
+            <div className="text-center">
+              <img src="/images/verticals/cp.png" alt="Competitive Programming" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Competitive <br />Programming</p>
+            </div>
+
+            {/* App Development */}
+            <div className="text-center">
+              <img src="/images/verticals/appdev.png" alt="App Development" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">App <br />Development</p>
+            </div>
+
+            {/* AI/ML */}
+            <div className="text-center">
+              <img src="/images/verticals/ai.png" alt="AI & ML" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">AI/ML</p>
+            </div>
+
+            {/* Public Relations */}
+            <div className="text-center">
+              <img src="/images/verticals/pr.png" alt="Public Relations" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Public Relations</p>
+            </div>
+
+            {/* Event Management */}
+            <div className="text-center">
+              <img src="/images/verticals/eventmang.png" alt="Event Management" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Event <br />Management</p>
+            </div>
+
+            {/* Design */}
+            <div className="text-center">
+              <img src="/images/verticals/design.png" alt="Design" className="w-16 h-16 sm:w-20 sm:h-20" />
+              <p className="text-sm sm:text-base md:text-[1.1rem] mt-2">Design</p>
+            </div>
           </div>
         </section>
 
         {/* Join Section */}
         <section className="join w-full flex flex-col gap-4 items-center text-center mt-10">
-          <h2 className="text-[30px] sm:text-[32px] font-[montserratb]">
+          <h2 className="text-[28px] sm:text-[32px] font-[montserratb]">
             JOIN THE <span className="text-orange-600">IETE</span> MOVEMENT!
           </h2>
           <div className="message-box text-black p-6 bg-white rounded-[8px] mt-4">
-            <p className="text-base sm:text-lg lg:text-xl xl:text-2xl text-start font-[montserrat]">
+            <p className="text-base sm:text-lg lg:text-xl xl:text-[1.3rem] text-start font-[montserrat]">
               As we continue to innovate and create, we invite you to be a part of this exciting journey. IETE Amrita is more than just a club; it&apos;s a community of passionate individuals shaping the future of technology. Don&apos;t miss out on the opportunity to collaborate, grow, and lead in the tech world.
               <br />
               <Link href="/team" passHref>Learn More
-                <span className="text-orange-600 font-[montserratb]"> About Our Team</span></Link>
+                <span className="text-orange-600 font-[montserratb] btn-primary"> About Our Team</span></Link>
             </p>
+          </div>
+
+          {/* Social Media Icons */}
+          <div className="flex justify-center items-center space-x-8 w-full max-w-[300px] sm:max-w-[400px] lg:max-w-[500px] mt-6">
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/footer/instagram.png" alt="Instagram" className="w-12 h-12 sm:w-13 sm:h-13" />
+            </a>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/footer/github.png" alt="Github" className="w-14 h-14 sm:w-13 sm:h-13" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/footer/linkedin.png" alt="LinkedIn" className="w-10 h-10 sm:w-13 sm:h-13" />
+            </a>
+            <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/footer/whatsapp.png" alt="Whatsapp" className="w-12 h-12 sm:w-13 sm:h-13" />
+            </a>
           </div>
         </section>
       </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -32,10 +32,72 @@ export default function RootLayout({ children }) {
         className={`${montserrata.variable} ${montserrat.variable} ${montserratb.variable} antialiased`}
       >
         {/* Main layout container */}
-        <div className="flex flex-col items-center justify-center min-h-screen px-9 py-9 bg-white text-black max-md:px-5">
+        <div className="flex flex-col min-h-screen px-9 py-9 bg-white text-black max-md:px-5">
           {/* Content container */}
-          <div className="flex flex-col px-12 pt-10 pb-24 w-full bg-stone-900 text-white rounded-[29px] max-md:px-5 max-md:pb-16 max-md:max-w-full">
+          <div className="flex flex-col px-12 pt-10 pb-24 w-full bg-stone-900 text-white rounded-[29px] max-md:px-5 max-md:pb-16 max-md:max-w-full flex-grow">
             {children}
+          </div>
+        </div>
+
+        {/* Footer */}
+        <div className="footer w-full text-white bg-stone-900">
+          <div className="flex flex-wrap justify-between items-center py-2 sm:px-12 max-md:flex-col max-md:items-center max-md:text-center max-md:space-y-4 max-md:py-6">
+            {/* Left Section */}
+            <div className="left flex flex-col justify-center mb-2 sm:mb-0 max-md:items-center">
+              <h2 className="text-[1.4rem] sm:text-[2.5rem] font-[montserratb]">Let's Connect!</h2>
+              <p className="text-[0.9rem] sm:text-[1.4rem] font-[montserrat]">ietesf@cb.amrita.edu</p>
+              <div className="flex justify-start items-center space-x-4 sm:space-x-6 mt-4 sm:mt-6 max-md:justify-center">
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/images/footer/instagram.png"
+                    alt="Instagram"
+                    className="w-10 h-10 sm:w-13 sm:h-13"
+                  />
+                </a>
+                <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/images/footer/github.png"
+                    alt="Github"
+                    className="w-10 h-10 sm:w-13 sm:h-13"
+                  />
+                </a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/images/footer/linkedin.png"
+                    alt="LinkedIn"
+                    className="w-9 h-9 sm:w-13 sm:h-13"
+                  />
+                </a>
+                <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/images/footer/whatsapp.png"
+                    alt="Whatsapp"
+                    className="w-10 h-10 sm:w-13 sm:h-13"
+                  />
+                </a>
+              </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="right flex flex-col font-[montserrat] py-6 justify-center max-md:items-center">
+              <div className="mb-2 justify-center sm:justify-end md:ml-10 lg:ml-10">
+                <img
+                  src="/Assets/logo.png"
+                  alt="App Development"
+                  className="w-24 h-24 sm:w-20 sm:h-20 md:w-24 md:h-24"
+                />
+              </div>
+              <div className="text-white">
+                <p className="text-[1rem] sm:text-base md:text-[1.2rem] mt-2">IETE Student Forum</p>
+                <p className="text-orange-400 text-[0.9rem] sm:text-base md:text-[1.2rem]">
+                  Amrita Coimbatore
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-white text-center text-black py-3 font-[montserrat] text-[0.8rem] sm:text-base">
+            <p className="text-[0.9rem] sm:text-base md:text-[1.1rem]">Made with 🧡 by IETE Amrita Team 2024-25</p>
           </div>
         </div>
       </body>
