682cce9b943df2c56d867b731fa01f5ecb700584
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2025-01-26 01:55:39 +0530

    fix: Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        'ipad': '820px',
+        'custom-sm': {'max': '1024px'},
+        'custom-md': {'max': '820px'},
+      },
+    },
   },
   plugins: [],
 };
