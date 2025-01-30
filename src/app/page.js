005e4fc1a1025c06f9abac23a22540364963e2fd
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2025-01-30 15:03:44 +0530

    fix: update page.js to fix that shadow

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect } from "react";
 import Maincontent from "./components/herolanding.jsx";
 import Events from "./components/Event/page.jsx";
@@ -8,12 +8,9 @@ export default function Home() {
   useEffect(() => {
     const hash = window.location.hash;
     if (hash === "#events-section") {
-      
       setTimeout(() => {
         const element = document.getElementById("events-section");
-        console.log(element)
         if (element) {
-          
           element.scrollIntoView({ behavior: "smooth" });
         }
       }, 100); // Delay of 100ms
@@ -22,7 +19,12 @@ export default function Home() {
 
   return (
     <>
-      <div className="min-h-screen p-4 pb-10 sm:p-10 bg-1c1917">
+      <div
+  className="min-h-screen p-4 pb-8 sm:p-10"
+  style={{
+    boxShadow: "0px 4px 6px rgba(28, 25, 23, 0.2), 0px 1px 3px rgba(28, 25, 23, 0.1)",
+  }}
+>
         <Maincontent />
       </div>
       <Events />
