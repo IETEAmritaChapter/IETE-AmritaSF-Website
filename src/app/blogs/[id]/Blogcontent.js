cb0c4435738a2162aa4164d9204df1ad97ecd45b
Eshwanth Karti T R <eshwanthkartitr@gmail.com>
2025-02-20 07:54:07 +0530

    modified:   public/Blogs/Intro_to_ai/content.md 	modified:   src/app/blogs/[id]/Blogcontent.js

--- src/app/blogs/[id]/Blogcontent.js
+++ src/app/blogs/[id]/Blogcontent.js
@@ -57,10 +57,10 @@ const BlogContent = ({ blogData }) => {
 
   const components = {
     h2: ({ children }) => (
-      <h2 className="!text-5xl [font-family:var(--font-montserratb)] !font-bold !mt-8 !mb-4 !text-white text-center">{children}</h2>
+      <h2 className="!text-2xl md:!text-3xl [font-family:var(--font-montserratb)] !font-bold !mt-8 !mb-4 !text-white text-center">{children}</h2>
     ),
     h3: ({ children }) => (
-      <h3 className="!text-3xl !font-semibold [font-family:var(--font-montserratb)] !mt-6 !mb-3 !text-white text-center">{children}</h3>
+      <h3 className="!text-xl md:!text-2xl !font-semibold [font-family:var(--font-montserratb)] !mt-6 !mb-3 !text-white text-center">{children}</h3>
     ),
     img: ({ node, ...props }) => (
       <div className="flex justify-center">
@@ -103,13 +103,13 @@ const BlogContent = ({ blogData }) => {
       );
     },
     p: ({ children }) => (
-      <p className="!text-lg [font-family:var(--font-montserrat)] text-justify !leading-relaxed !mb-6 !text-gray-300 ">{children}</p>
+      <p className="!text-base md:!text-lg [font-family:var(--font-montserrat)] text-justify !leading-relaxed !mb-6 !text-gray-300 ">{children}</p>
     ),
     ul: ({ children }) => (
       <ul className="!list-disc [font-family:var(--font-montserrat)] !list-inside !space-y-2 !mb-6 ">{children}</ul>
     ),
     li: ({ children }) => (
-      <li className="!text-lg [font-family:var(--font-montserrat)] !leading-relaxed !text-gray-300">{children}</li>
+      <li className="!text-base md:!text-lg [font-family:var(--font-montserrat)] !leading-relaxed !text-gray-300">{children}</li>
     ),
     table: ({ children }) => (
       <div className="overflow-x-auto">
@@ -147,9 +147,9 @@ const BlogContent = ({ blogData }) => {
   }
 
   return (
-    <div className="text-white container mx-auto px-4 py-16">
+    <div className="text-white container mx-auto px-4 py-8 md:py-16">
       <div className="mb-6">
-        <div className="mx-auto w-[250px] max-w-2xl">
+        <div className="mx-auto w-full max-w-2xl">
           <img
             src={blog.metadata.preview_image}
             alt={blog.metadata.title}
@@ -157,7 +157,7 @@ const BlogContent = ({ blogData }) => {
           />
         </div>
       </div>
-      <h1 className="text-4xl md:text-5xl [font-family:var(--font-montserratb)] font-bold text-center mb-8">
+      <h1 className="text-2xl md:text-5xl [font-family:var(--font-montserratb)] font-bold text-center mb-8">
         {blog.metadata.title}
       </h1>
       <p className="text-center text-gray-400 mb-6">{blog.metadata.date} </p>
